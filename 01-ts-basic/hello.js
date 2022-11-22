9872523e918727c3803cc8c96f10192f3d2e2ab0
chenhuiyi <chenhuiyi@qinsilk.com>
2022-11-22 14:27:08 +0800

    11.22下午学习TypeScript

---
+++ 01-ts-basic/hello.js
@@ -0,0 +1,9 @@
+"use strict";
+// 你好，世界
+// console.log('Hello World');
+function greet(person, date) {
+    console.log(`Hello ${person}, today is ${date}`);
+}
+greet('lulu', new Date());
+// let msg = 'hello world'
+// msg = 32   
---
+++ 01-ts-basic/hello.ts
@@ -0,0 +1,11 @@
+// 你好，世界
+// console.log('Hello World');
+
+function greet(person: string, date: Date){
+  console.log(`Hello ${person}, today is ${date}`);
+}
+
+greet('lulu', new Date())
+
+// let msg = 'hello world'
+// msg = 32   
